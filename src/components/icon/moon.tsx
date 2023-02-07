993be72ba6607d537c727abf322db313397f3b05
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-06 22:30:04 -0300

    set each icon to its correspondant dir and fix *FINALLY* issue related to themes by setting dark class on each icon

---
+++ src/components/icon/moon.tsx
@@ -0,0 +1,16 @@
+export  function MoonIcon() {
+	return (
+		<svg
+			fill="currentColor"
+			stroke-width="0"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 512 512"
+			height="1em"
+			width="1em"
+			aria-label="Turn dark mode"
+			class="h-6 w-6 hover:opacity-60  duration-100 ease-in-out dark:block hidden"
+		>
+			<path d="M32 256C32 132.2 132.3 32 255.8 32c11.36 0 29.7 1.668 40.9 3.746 9.616 1.777 11.75 14.63 3.279 19.44C245 86.5 211.2 144.6 211.2 207.8c0 109.7 99.71 193 208.3 172.3 9.561-1.805 16.28 9.324 10.11 16.95C387.9 448.6 324.8 480 255.8 480 132.1 480 32 379.6 32 256z"></path>
+		</svg>
+	)
+}
